8b595de9e3fade679367f9e9866b55f168bfb002
will <cg01will@gmailcom>
2018-06-27 22:17:56 +0200

    "IOTA TIP" Button added beside the "Subscribe" Button on Youtube

--- content.js
+++ content.js
@@ -1,1 +1,12 @@
-alert("worked");
+// Get the Position where to add the Button
+let metaContents = document.getElementById("meta-contents");
+let topRow = metaContents.childNodes[0].childNodes[0].childNodes[1];
+
+// Create the Button and Add it to the position
+let btn = document.createElement("BUTTON");
+btn.innerHTML = "IOTA Tip";
+topRow.appendChild(btn);
+
+// Get the description of the video
+let description = document.getElementById("description").innerHTML;
+//console.log(description);
